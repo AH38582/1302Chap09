ab6a14b9196e4bd1e76a50a72521f4a134a17f00
Asia Hayden <ah38582@georgiasouthern.edu>
2026-08-19 10:23:33 -0400

    added methods

--- Chapter09/src/Circle.java
+++ Chapter09/src/Circle.java
@@ -2,5 +2,22 @@
 public class Circle {
 //	default data member radius
 	double radius = 1.0;
+	
+//	behaviors for circle lines 8-18
+	
+	double getDiameter() {
+		return Math.PI * radius * radius;
+	}
+	
+	double getArea() {
+		return Math.PI * radius * radius;
+	}
+	
+	double getPerimeter() {
+		return Math.PI * radius * 2;
+	}
+	
+	
+	
 
 }
