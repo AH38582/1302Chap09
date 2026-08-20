0941836319f4fb8728c2bc8991864120dc24b00c
Asia Hayden <ah38582@georgiasouthern.edu>
2026-08-19 21:07:28 -0400

    Updated CircleTest class

--- Chapter09/src/CircleTest.java
+++ Chapter09/src/CircleTest.java
@@ -3,7 +3,11 @@ public class CircleTest {
 
 	public static void main(String[] args) {
 //		main methods are where object instances are created
-		Circle circ = new Circle();
+		Circle circ1 = new Circle();
+		
+		Circle circ2 = new Circle(2.0);
+		
+		System.out.println(circ2.getRadius());
 
 	}
 
