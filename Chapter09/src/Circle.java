ec779b948148f478cd4d87beb8756be5a5127046
Asia Hayden <ah38582@georgiasouthern.edu>
2026-08-24 09:11:46 -0400

    restored

--- Chapter09/src/Circle.java
+++ Chapter09/src/Circle.java
@@ -1,31 +1,15 @@
-// Defining public class Circle for object circle
+
 public class Circle {
-	
-//	data members
 	private double radius;
 	
-//	default constructor
-	public Circle () {
+	public Circle() {
+		System.out.println("New circle created");
 		radius = 1.0;
 	}
 	
-//	Convenience constructor
-	public Circle (double radius) {
+	public Circle(int radius) {
+		System.out.println("New circle created");
 		this.radius = radius;
-	}
-	
-//	behaviors for circle
-	
-	double getDiameter() {
-		return Math.PI * radius * radius;
-	}
-	
-	double getArea() {
-		return Math.PI * radius * radius;
-	}
-	
-	double getPerimeter() {
-		return Math.PI * radius * 2;
 	}
 
 	public double getRadius() {
@@ -36,9 +20,14 @@ public class Circle {
 		this.radius = radius;
 	}
 	
+	public double getArea() {
+		return Math.PI * radius * radius;
+	}
+	
+	public double getPerimeter() {
+		
+	}
 	
 	
 	
-	
-
 }
