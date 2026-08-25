39e49a964d4a74e1244faca20b688ac16217b980
Asia Hayden <ah38582@georgiasouthern.edu>
2026-08-24 21:51:31 -0400

    GregorianCalendarTest class

---
+++ Chap09/src/GregorianCalenderTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class GregorianCalenderTest {
+
+	public static void main(String[] args) {
+		java.util.GregorianCalendar cal1 = new GregorianCalendar();
+		
+		System.out.printf("%d/%d/%d", cal1.get(GregorianCalendar.MONTH), cal1.get(GregorianCalendar.DAY_OF_MONTH), cal1.get(GregorianCalendar.YEAR));
+
+	}
+
+}
