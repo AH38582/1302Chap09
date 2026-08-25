b59e7dd1549d2e2ff1a5d552b63b21631e4c28bd
Asia Hayden <ah38582@georgiasouthern.edu>
2026-08-24 21:44:29 -0400

    Updated RandomTest class

--- Chap09/src/RandomTest.java
+++ Chap09/src/RandomTest.java
@@ -6,19 +6,26 @@ public class RandomTest {
 		for (int i = 0; i < 10; i++) {
 			System.out.print(generate1.nextInt(10) + " ");
 		}
-		
+
 		System.out.println();
-		
+
 		java.util.Random generate2 = new java.util.Random(1);
 		for (int i = 0; i < 10; i++) {
 			System.out.print(generate2.nextInt(10) + " ");
 		}
-		
+
 		System.out.println();
-		
+
 		java.util.Random generate3 = new java.util.Random(3);
 		for (int i = 0; i < 10; i++) {
 			System.out.print(generate3.nextInt(10) + " ");
+		}
+		
+		System.out.println();
+
+		java.util.Random generate4 = new java.util.Random(1000);
+		for (int i = 0; i < 10; i++) {
+			System.out.print(generate3.nextInt(100) + " ");
 		}
 	}
 
